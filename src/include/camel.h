4e626e5ff290d3728376f05d3e41576386f04f97
pic <llm@stu.pku.edu.cn>
2026-05-18 15:01:01 +0800

    Add main camel.h umbrella header
    - Export public headers for all modules (estimator, detector, burst, common, sender, receiver)

---
+++ src/include/camel.h
@@ -0,0 +1,17 @@
+/*-
+* Copyright (c) 2017-2018 Razor, Inc.
+*	All rights reserved.
+*
+* See the file LICENSE for redistribution information.
+*/
+#ifndef __camel_h_
+#define __camel_h_
+
+#include "bandwidth_estimator.h"
+#include "congestion_detector.h"
+#include "burst_controller.h"
+#include "common.h"
+#include "receiver.h"
+#include "sender.h"
+
+#endif
